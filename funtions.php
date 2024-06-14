06388146a4aa3b538473e7babb359cb859cc000d
zero <scheepstrabas@icloud.com>
2024-06-14 09:07:29 +0200

    pagina's toegevoegd

--- funtions.php
+++ funtions.php
@@ -1,1 +1,2 @@
 <?php
+wp_enqueue_style('style', get_stylesheet_uri());
--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php
+<?php require "header.php" ?>
+<?php wp_nav_menu() ?>
+
+<h1>welkome</h1>
+
+
+<?php wp_footer() ?>
+<?php require "footer.php" ?>
+
