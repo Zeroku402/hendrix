7530d597b2a626ecc8f4ad4a232e73babdf717a1
zero <scheepstrabas@icloud.com>
2024-06-14 09:22:51 +0200

    home pagina begonnen

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php require "header.php" ?>
 <?php wp_nav_menu() ?>
+<div class="info.pagina"></div>
+<h1>welkom</h1>
+<p>Niek Hendrix has been working on an innovative oeuvre in a menacing pace,
+    constantly seeking images that should be understood to understand ourselves.
+    His distinctive style of photographic black and white paintings and bold colour interpretations of 17th century Dutch paintings,
+    are shown in thoughtful constellations that challenge the viewer.
+    Hendrix is exhibited at prestigious places as the Royal Palace Amsterdam, Dordrechts Museum, Amsterdams Museum and exhibited in Basel, New York, Moscow and San Francisco.
 
-<h1>welkome</h1>
+    Hendrix considers existing images, iconic or not, as an open-source.
+    He activates them by repeating them and by placing them in a different context.
+    This appropriation is not to make an exact copy but to create a different image of it.
+    He paints them with a different brushstroke, a different paint, colours are let go off,
+    appearing in shades of black and white, and also the dimensions are different from the originals.
+    He places them in front of works from different times or adds a work of his own hands.
+    In unexpected constellations he puts the viewer to the test: they must not only look again but also give meaning again.</p>
+<img src="images/home%20foto%201.webp">
+<img src="images/home%20pagina%202.webp">
 
 
 <?php wp_footer() ?>
