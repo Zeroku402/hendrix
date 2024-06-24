41fe27682e97f34f3187d33e0d9c56028922f1a5
Ilie <9015672@student.zadkine.nl>
2024-06-24 08:43:22 +0200

    naam aangepast aan bestanden

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php require "header.php" ?>
-<?php wp_nav_menu() ?>
-<?php echo get_the_content() ?>
-<?php wp_footer() ?>
+<?php get_header(); ?>
+<?php wp_nav_menu(); ?>
+<?php echo get_the_content(); ?>
+<?php wp_footer(); ?>
 <?php require "footer.php" ?>
 
